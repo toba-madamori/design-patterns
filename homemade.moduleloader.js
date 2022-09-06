f4daa79f9ca96ce5a1ec4b82f5ed0d23486e1263
Toba <tobajamezmadamori@gmail.com>
2022-09-06 15:43:23 +0100

    changed to ESM

--- homemade.moduleloader.js
+++ homemade.moduleloader.js
@@ -1,32 +1,32 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-// loads the content of a module, wraps it into a private scope and evaluates it.
-function loadModule (filename, module, require){
-    const wrappedSrc =`(function (module, exports, require) 
-    { ${fs.readFileSync(filename, 'utf8')} })(module, module.exports, require)` 
-    eval(wrappedSrc)
-}
+// // loads the content of a module, wraps it into a private scope and evaluates it.
+// function loadModule (filename, module, require){
+//     const wrappedSrc =`(function (module, exports, require) 
+//     { ${fs.readFileSync(filename, 'utf8')} })(module, module.exports, require)` 
+//     eval(wrappedSrc)
+// }
 
-//sample require function
-function sampleRequire(moduleName) {
-    console.log(`Require invoked for module: ${moduleName}`) 
-    const id = sampleRequire.resolve(moduleName) 
-    if (sampleRequire.cache[id]) {
-        return sampleRequire.cache[id].exports 
-    }
-        // module metadata 
-    const module = {
-        exports: {}, 
-        id
-    }
-        // Update the cache 
-    sampleRequire.cache[id] = module
-        // load the module     
-    loadModule(id, module, sampleRequire)
-        // return exported variables 
-        return module.exports
-}
-sampleRequire.cache = {} 
-sampleRequire.resolve = (moduleName) => {
-    /* resolve a full module id from the moduleName */ 
-}
+// //sample require function
+// function sampleRequire(moduleName) {
+//     console.log(`Require invoked for module: ${moduleName}`) 
+//     const id = sampleRequire.resolve(moduleName) 
+//     if (sampleRequire.cache[id]) {
+//         return sampleRequire.cache[id].exports 
+//     }
+//         // module metadata 
+//     const module = {
+//         exports: {}, 
+//         id
+//     }
+//         // Update the cache 
+//     sampleRequire.cache[id] = module
+//         // load the module     
+//     loadModule(id, module, sampleRequire)
+//         // return exported variables 
+//         return module.exports
+// }
+// sampleRequire.cache = {} 
+// sampleRequire.resolve = (moduleName) => {
+//     /* resolve a full module id from the moduleName */ 
+// }
