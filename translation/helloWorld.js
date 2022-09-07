d91b9b65eb7effe44d7f5049d8e4849be2252a60
Toba <tobajamezmadamori@gmail.com>
2022-09-07 15:20:22 +0100

    playing around with dynamic imports

---
+++ translation/helloWorld.js
@@ -0,0 +1,13 @@
+const SUPPORTED_LANGUAGES = ['el', 'en', 'es', 'it', 'pl'] 
+
+const selectedLanguage = process.argv[2]
+
+if (!SUPPORTED_LANGUAGES.includes(selectedLanguage)) {
+    console.error('The specified language is not supported') 
+    process.exit(1)
+}
+const translationModule = `./strings-${selectedLanguage}.js`
+
+// dynamic imports with the import keyword
+import(translationModule) 
+    .then((strings) => { console.log(strings.HELLO) }) 
