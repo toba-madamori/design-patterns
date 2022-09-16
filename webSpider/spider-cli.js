58a1c4e3f32da826826e5395e5b0688907c0b0be
Toba <tobajamezmadamori@gmail.com>
2022-09-16 15:06:34 +0100

    playing around with callbacks, created a simple webcrawler or spider

---
+++ webSpider/spider-cli.js
@@ -0,0 +1,11 @@
+import { spider } from "./v1.js"
+
+spider(process.argv[2], (err, filename, downloaded)=>{
+    if(err){
+        console.error(err)
+    }else if(downloaded){
+        console.log(`completed the download of "${filename}"`)
+    }else{
+        console.log(`"${filename}" has already been downloaded`)
+    }
+})
