724d25aa495aaf5a6e1482864393352f21259a7b
Toba <tobajamezmadamori@gmail.com>
2022-09-24 19:55:34 +0100

    modification: updated our spider, implemented a task-queue to limit concurrency

--- webSpider/spider-cli.js
+++ webSpider/spider-cli.js
@@ -1,13 +1,13 @@
-import { spider } from "./v4.js"
+import { spider } from './v5.js'
+import { TaskQueue } from './taskQueue.js'
 
 const url = process.argv[2]
 const nesting = Number.parseInt(process.argv[3], 10) || 1
 
-spider(url, nesting, err => {
-    if (err) {
-        console.error(err)
-        process.exit(1)
-    }
+const concurrency = Number.parseInt(process.argv[4], 10) || 2
 
-    console.log('Download complete')
-})
+const spiderQueue = new TaskQueue(concurrency)
+spiderQueue.on('error', console.error)
+spiderQueue.on('empty', () => console.log('Download complete'))
+
+spider(url, nesting, spiderQueue)
--- webSpider/v5.js
+++ webSpider/v5.js
@@ -26,52 +26,50 @@ const download = (url, filename, cb) => {
     })
 }
 
-export function spider (url, nesting, cb) {
-    const spidering = new Set()
+const spiderLinks = (currentUrl, body, nesting, queue) => {
+    if (nesting === 0) {
+        return
+    }
+
+    const links = getPageLinks(currentUrl, body)
+    if (links.length === 0) {
+        return
+    }
+
+    links.forEach(link => spider(link, nesting - 1, queue))
+}
+
+function spiderTask (url, nesting, queue, cb) {
     const filename = urlToFilename(url)
-    fs.readFile(filename, 'utf-8', (err, fileContent) => {
+    fs.readFile(filename, 'utf8', (err, fileContent) => {
         if (err) {
             if (err.code !== 'ENOENT') {
                 return cb(err)
             }
-            if (spidering.has(url)) {
-                return process.nextTick(cb)
-            }
-            spidering.add(url)
 
             return download(url, filename, (err, requestContent) => {
                 if (err) {
                     return cb(err)
                 }
-                spiderLinks(url, requestContent, nesting, cb)
+
+                spiderLinks(url, requestContent, nesting, queue)
+                return cb()
             })
         }
-        spiderLinks(url, fileContent, nesting, cb)
+
+        spiderLinks(url, fileContent, nesting, queue)
+        return cb()
     })
 }
 
-const spiderLinks = (currentUrl, body, nesting, cb) => {
-    if (nesting === 0) {
-        return process.nextTick(cb)
+const spidering = new Set()
+export function spider (url, nesting, queue) {
+    if (spidering.has(url)) {
+        return
     }
 
-    const links = getPageLinks(currentUrl, body)
-    if (links.length === 0) {
-        return process.nextTick(cb)
-    }
-
-    let completed = 0
-    let hasErrors = false
-
-    function done (err) {
-        if (err) {
-            hasErrors = true
-            return cb(err)
-        }
-        if (++completed === links.length && !hasErrors) {
-            return cb()
-        }
-    }
-
-    links.forEach(link => spider(link, nesting - 1, done))
+    spidering.add(url)
+    queue.pushTask((done) => {
+        spiderTask(url, nesting, queue, done)
+    })
 }
