e466c22079e15b2f1bd230bfa3d0ca40b8d9c447
Toba <tobajamezmadamori@gmail.com>
2022-10-12 15:56:11 +0100

    iterable example of our read stream

--- Streams/Readable/read-stdin.js
+++ Streams/Readable/read-stdin.js
@@ -9,9 +9,19 @@
 //     })
 //     .on('end', () => console.log('End of stream'))
 
-process.stdin
-    .on('data', (chunk) => {
+// process.stdin
+//     .on('data', (chunk) => {
+//         console.log('New data available')
+//         console.log(`Chunk read (${chunk.length} bytes): "${chunk.toString()}"`)
+//     })
+//     .on('end', () => console.log('End of stream'))
+
+async function main () {
+    for await (const chunk of process.stdin) {
         console.log('New data available')
-        console.log(`Chunk read (${chunk.length} bytes): "${chunk.toString()}"`)
-    })
-    .on('end', () => console.log('End of stream'))
+        console.log(`Chunk read (${chunk.length} bytes): "${chunk.toString()}`)
+    }
+    console.log('End of stream')
+}
+
+main()
